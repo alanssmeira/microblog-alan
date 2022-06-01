e22c3e46e126154ef768626d27f0ff300572b162
alanssmeira <alan.sobreirasa@gmail.com>
2022-06-01 15:40:06 -0300

    criar função de verificar se está logado

--- inc/funcoes-sessao.php
+++ inc/funcoes-sessao.php
@@ -3,4 +3,18 @@
 os recursos de login/logout e verificação
 de permissão de acesso dos usuários */
 
+/* VERIFICANDO SE NÃO EXISTE UMA SESSÃO EM FUNCIONAMENTO */
 
+if (!isset($_SESSION)) {
+    session_start();
+}
+
+function verificaAcesso(){
+    /* Se NÃO existe uma variável de sessão logada */
+    if (!isset($_SESSION['id'])) {
+        /* então significa que ele NÃO ESTÁ LOGADO, portanto apague qualquer resquício de sessão e force o usuário a ir para o login.php */
+        session_destroy();
+        header("location:../login.php");
+        die();
+    }
+}
