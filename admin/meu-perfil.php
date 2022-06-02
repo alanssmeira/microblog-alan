38f81ef632a28fa890325bd1670e990dafff7dc8
alanssmeira <alan.sobreirasa@gmail.com>
2022-06-02 15:48:44 -0300

    Update meu-perfil.php
    recuperar dados do usuário logado no form para atualizá-los

--- admin/meu-perfil.php
+++ admin/meu-perfil.php
@@ -1,5 +1,10 @@
 <?php 
-require "../inc/cabecalho-admin.php"; 
+require "../inc/cabecalho-admin.php";
+require "../inc/funcoes-usuarios.php";
+
+/* Carregamos os dados da pessoa logada na sessão ($_SESSION['id'
+]) */
+$usuario = lerUmUsuario($conexao, $_SESSION['id']);
 
 ?>
   <div class="row">
@@ -7,29 +12,18 @@ require "../inc/cabecalho-admin.php";
       <h2 class="text-center">Atualizar meu perfil</h2>
 
       <form action="" method="post" id="form-atualizar" name="form-atualizar" class="mx-auto w-75">
-        <input class="form-control" type="hidden" name="id">
 
         <div class="form-group">
           <label for="nome">Nome:</label>
-          <input class="form-control" required type="text" id="nome" name="nome" placeholder="Nome (obrigatório)">
+          <input value="<?=$usuario['nome']?>" class="form-control" required type="text" id="nome" name="nome" placeholder="Nome (obrigatório)">
         </div>
         <div class="form-group">
           <label for="email">E-mail:</label>
-          <input class="form-control" required type="email" id="email" name="email" placeholder="E-mail (obrigatório)">
+          <input value="<?=$usuario['email']?>" class="form-control" required type="email" id="email" name="email" placeholder="E-mail (obrigatório)">
         </div>
         <div class="form-group">
           <label for="senha">Senha</label>
           <input class="form-control" type="password" id="senha" name="senha"  placeholder="Preencha apenas se for alterar">
-        </div>
-
-        <!-- Select de tipos/níveis de usuários -->
-        <div class="form-group">
-          <label for="tipo">Tipo:</label>
-          <select name="tipo" id="tipo" required class="custom-select">
-              <option value=""></option>                    
-              <option value="editor">Editor</option>     
-              <option value="admin">Administrador</option>
-          </select>
         </div>
 
         <button class="btn btn-lg btn-primary" name="atualizar">Atualizar</button>
