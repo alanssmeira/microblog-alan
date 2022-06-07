bd0bae3a2cbc7d873659b38b0d20c51c4c3fcbe7
alanssmeira <alan.sobreirasa@gmail.com>
2022-06-07 16:55:58 -0300

    acionar função lerPosts na pág de posts

--- admin/posts.php
+++ admin/posts.php
@@ -1,11 +1,18 @@
 <?php 
+require "../inc/funcoes-posts.php";
 require "../inc/cabecalho-admin.php"; 
+
+// Recuperando dados da pessoa logada na sessão
+$posts = lerPosts($conexao, $_SESSION['id'], $_SESSION['tipo']);
+
+// contar posts
+$quantidade = count($posts);
 
 ?>      
     
 <div class="row">
   <article class="col-12 bg-white rounded shadow my-1 py-4">
-    <h2 class="text-center">Posts <span class="badge badge-primary">X</span></h2>
+    <h2 class="text-center">Posts <span class="badge badge-primary"><?=$quantidade?></span></h2>
     <p class="lead text-right">
       <a class="btn btn-primary" href="post-insere.php">Inserir novo post</a>
     </p>
@@ -23,25 +30,30 @@ require "../inc/cabecalho-admin.php";
         </thead>
       
         <tbody>
-
+        <?php foreach ($posts as $post) { ?>
           <tr>
-            <td> Título do post... </td>
-            <td> 21/12/2112 21:12 </td>
-            <td> Autor do post... </td>
+            <td> <?=$post['titulo']?> </td>
+            <td> <?=$post['data']?> </td>
+
+            <!-- Restringe ao admin, acesso ao usuario que criou o post -->
+            <?php if($_SESSION['tipo'] == 'admin'){ ?>
+            <td> <?=$post['autor']?> </td>
+            <?php } ?>
+
             <td class="text-center">
               <a class="btn btn-warning btn-sm" 
-              href="post-atualiza.php">
+              href="post-atualiza.php?id=<?=$post['id']?>">
                   Atualizar
               </a>
             </td>
             <td class="text-center">
               <a class="btn btn-danger btn-sm excluir"
-              href="post-exclui.php">
+              href="post-exclui.php?id=<?=$post['id']?>">
                   Excluir
               </a>
             </td>
           </tr>
-
+          <?php } ?>
         </tbody>                
       </table>
       
