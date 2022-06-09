6b573bebe969f9991f537012ce79a0152800c649
alanssmeira <alan.sobreirasa@gmail.com>
2022-06-09 15:57:16 -0300

    págs pesquisa, posts, post

--- inc/funcoes-posts.php
+++ inc/funcoes-posts.php
@@ -129,9 +129,9 @@ function formataData(string $data):string {
 
 /*** Funções para a área PÚBLICA do site ***/
 
-/* Usada em index.php */
+/* Usada em index.php */        // dispensa mais parâmetros pq é publica
 function lerTodosOsPosts(mysqli $conexao):array {
-    $sql = "";
+    $sql = "SELECT id, imagem, titulo, resumo FROM posts ORDER BY data DESC";
     
     $resultado = mysqli_query($conexao, $sql) or die(mysqli_error($conexao));
     $posts = [];
@@ -144,8 +144,9 @@ function lerTodosOsPosts(mysqli $conexao):array {
 
 
 /* Usada em post-detalhe.php */
-function lerDetalhes(mysqli $conexao):array {    
-    $sql = "";
+function lerDetalhes(mysqli $conexao, int $idPost):array {    
+    $sql = "SELECT posts.imagem, posts.titulo, posts.texto, posts.data, usuarios.nome AS autor FROM posts INNER JOIN usuarios ON posts.usuario_id = usuarios.id WHERE posts.id = $idPost";
+    //INNER JOIN traz dados de 2 ou mais tabelas e precisa de critérios, no ON
 
     $resultado = mysqli_query($conexao, $sql) or die(mysqli_error($conexao));
     return mysqli_fetch_assoc($resultado); 
@@ -154,9 +155,12 @@ function lerDetalhes(mysqli $conexao):array {
 
 
 /* Usada em search.php */
-function busca($conexao):array {
-    $sql = "";
-        
+function busca(mysqli $conexao, string $termo):array {
+    
+    // LIKE é ideal para buscas, permite encontrar a palavra dentro uma frase mas apenas se começar com a palavra buscada
+    $sql = "SELECT id, titulo, data, resumo FROM posts WHERE titulo LIKE '%$termo%' OR resumo LIKE '%$termo%' OR texto LIKE '%$termo%' ORDER BY data DESC";
+    // A % (porcentagem) permite que a palavra seja encontrada em qualquer posição do texto    
+
     $resultado = mysqli_query($conexao, $sql) or die(mysqli_error($conexao));
     $posts = [];
     while( $post = mysqli_fetch_assoc($resultado) ){
